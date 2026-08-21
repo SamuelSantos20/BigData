d7bc0e996d4bd16967c9e709ea13c64acb69bd15
Samuel Santos Miranda <138618261+SamuelSantos20@users.noreply.github.com>
2026-08-21 19:56:53 -0300

    Alteração de pasta e adicionado o tratamento de erro.
    Adicionada uma pasta de exercício para cada pasta de aula, adicionado o arquivo da aula 5 e adicionada a estrutura de try except no desafio_2 da aula 5.

--- src/UC1/aula5/Desafios/Desafio_1.py
+++ src/UC1/aula5/Desafios/Desafio_1.py
@@ -6,31 +6,28 @@ Use o laço for para repetir a lógica de cálculo de média e status
 
 '''
 
-
-import src.UC1.aula4.Desafio_5 as desafio_5
-
 for x in range(10):
     for i in range(2):
         i += 1
         nota = float(input(f'Informe a {i}° nota: '))
-        desafio_5.notas.append(nota)
+        Desafio_5.notas.append(nota)
     opc = input('O aluno realizou a avaliação optativa? [S/N] ')
-    nota_1 = desafio_5.notas[0]
-    nota_2 = desafio_5.notas[1]
+    nota_1 = Desafio_5.notas[0]
+    nota_2 = Desafio_5.notas[1]
 
     if opc.upper() == 'S':
         nota_op = float(input('Informe a nota do aluno na avaliação optativa: '))
-        nota_min = min(desafio_5.notas)
+        nota_min = min(Desafio_5.notas)
         if nota_op > nota_min:
-            desafio_5.notas.remove(nota_min)
-            desafio_5.notas.append(nota_op)
-            print('Resultado: ' + desafio_5.avalia(nota_1, nota_2))
+            Desafio_5.notas.remove(nota_min)
+            Desafio_5.notas.append(nota_op)
+            print('Resultado: ' + Desafio_5.avalia(nota_1, nota_2))
         else:
             print('Nota optativa menor do que as duas primeiras notas do aluno, então foi desconsiderada.')
-            print('Resultado: ' + desafio_5.avalia(nota_1, nota_2))
+            print('Resultado: ' + Desafio_5.avalia(nota_1, nota_2))
 
     elif opc.upper() == 'N':
-        print('Resultado: ' + desafio_5.avalia(nota_1, nota_2))
+        print('Resultado: ' + Desafio_5.avalia(nota_1, nota_2))
 
     else:
         print('[Erro Desconhecido!]')
---
+++ src/UC1/aula5/aula05.py
@@ -0,0 +1,6 @@
+somador = int(input('Registro: '))
+controle = 0
+while controle <= 30:
+    controle = controle + somador
+    somador = int(input('Registro: '))
+print('Oficina Lotada')
