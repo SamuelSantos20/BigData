432e1f9565016ef6f213bf8fe2a20d6ab68b2754
Samuel Santos Miranda <138618261+SamuelSantos20@users.noreply.github.com>
2026-08-29 22:43:32 -0300

    feat(aula09): adiciona função atualizar_preparo_item com validações

---
+++ src/UC1/aula09/preparo_item.py
@@ -0,0 +1,31 @@
+def  atualizar_preparo_item(nome:str,quantidade: int = 1,prato:bool = False) -> dict:
+    """Atualiza o preparo de um item"""
+    try:
+        if not isinstance(nome,str) or not nome.strip():
+            raise ValueError("Nome inválido")
+
+        if not isinstance(quantidade,int) or quantidade <=0:
+            raise ValueError("Quantidade deve ser maior que zero")
+
+        if not isinstance(prato,bool):
+            raise ValueError("Prato deve ser booleano")
+        
+        item_atualizado = {
+            "nome":nome,
+            "quantidade":quantidade,
+            "prato":prato
+        }
+
+        return item_atualizado
+
+
+    except Exception as e:
+        print(e)
+        return {
+        "nome":None,
+        "quantidade":None,
+        "prato":None
+    }
+    
+
+    
