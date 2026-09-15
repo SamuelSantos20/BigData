e14a73b8517c958615c57a8a1b39dd6d75d9b26c
Samuel Santos Miranda <138618261+SamuelSantos20@users.noreply.github.com>
2026-09-14 21:34:38 -0300

    Aula 03
    Aula sobre analise de dados, uso da biblioteca numpy e matplotlib.pyplot e SQL.

--- src/UC2/Aula01/aula01.py
+++ src/UC2/Aula01/aula01.py
@@ -5,17 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import openpyxl as xl
 
+#numeros_impares = [43,55,1,3,11,27,109]
 
-numeros_impares = [43,55,1,3,11,27,109]
+#numeros_seq = [2,3,4,5,6,6,7]
 
-numeros_seq = [2,3,4,5,6,6,7]
-
-serie_impares = pd.Series(numeros_impares)
+#serie_impares = pd.Series(numeros_impares)
 
 #print(serie_impares)
 #print(type(serie_impares))
 
-print(serie_impares.sum())
+#print(serie_impares.sum())
 
 #print(serie_impares.mean())
 
@@ -59,7 +58,7 @@ print(type(tabela_filmes))
 '''
 ### LEITURA DE XLSX
 
-leitura_invest = pd.read_excel('base_invest.xlsx',1)
+'''leitura_invest = pd.read_excel('base_invest.xlsx',1)
 
 print(leitura_invest)
 
@@ -100,3 +99,45 @@ print(tabela_filmes.query('ano == 1995'))
 consulta1 = tabela_filmes.query('faturamento == 5.5')
 
 print(consulta1)
+
+'''
+
+#dados = np.array([12,15,17,20,22,25,28,30])
+
+#print(dados)
+
+'''
+#Calcular os quartis
+q1 = np.percentile(dados, 25) # O 1° quartil representa 25%dos dados 
+q2 = np.percentile(dados, 50) #A Mediana é o mesmo que o quartil de 50%
+q3 = np.percentile(dados, 75)# O terceiro maior quartil representa 75% dos dados 
+
+
+print(f'Meu primeiro quartil é (Q1): {q1}')
+print(f'Meu segundo quartil é (Q2, Mediana): {q2}')
+print(f'Meu terceiro quartil é (Q3): {q3}')
+
+'''
+
+df_transacoes = pd.read_excel('base_invest.xlsx',1)
+print(df_transacoes.head(10))
+
+q1_preco = df_transacoes['preco'].quantile(0.25)
+q2_preco = df_transacoes['preco'].quantile(0.50)
+q3_preco = df_transacoes['preco'].quantile(0.75)
+
+print(f'Preco Q1: {q1_preco}')
+
+print(f'Preco Mediana (Q2): {q2_preco}')
+
+print(f'Preco Q3: {q3_preco}')
+
+
+
+contagem_operacao = df_transacoes['operacao'].value_counts()
+
+contagem_operacao.plot(kind='bar',title='Tipos de Operação')
+
+#Mostrar o grafico
+
+plt.show()
---
+++ src/UC2/Aula03/aula03.sql
@@ -0,0 +1,5 @@
+-- CRIANDO MEU BANCO DE DADOS
+
+CREATE DATABASE meu_ecommerce;
+
+USE  meu_ecommerce;
